9e939cfc5883a70250b7849df2fcafe99841344b
Víctor Velarde <victor.velarde@gmail.com>
2017-03-29 01:24:48 +0200

    Simple components as functions

--- src/index.js
+++ src/index.js
@@ -3,18 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-//const element = <App />;
 
-function renderApp(){
-  const element = <div>
-    <h1>Hey, ho! Let's go!</h1>
-     <h2>It is {new Date().toLocaleTimeString()}.</h2>
-  </div>;
+const element = <div>
+  <h1>Hey, ho! Let's go!</h1>
+  <h2>It is {new Date().toLocaleTimeString()}.</h2>
+</div>;
 
-  ReactDOM.render(
-    element,
-    document.getElementById('root')
-  );
-}
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
 
-setInterval(renderApp, 1000);
+//setInterval(renderApp, 1000);
