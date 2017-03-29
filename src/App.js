1ad220bb6fb5d9f00f969cb8949d52eaa318a7c8
Víctor Velarde <victor.velarde@gmail.com>
2017-03-29 23:00:37 +0200

    Getting the basic ideas on elements and components

--- src/App.js
+++ src/App.js
@@ -8,26 +8,44 @@ function Welcome(props) {
 }
 
 function Menu (props){
-  const entries = () => props.entries.map(e => <li>{e}</li>);
-  console.log(entries);
   return(
-    <ul>
-      {entries()}
+    <ul className="Menu">
+      { props.entries.map(e => <li><a href="#">{e}</a></li>) }
     </ul>);
   }
-
-
+/*
+  function Comment(props) {
+    return (
+      <div className="Comment">
+        <div className="UserInfo">
+          <img className="Avatar"
+            src={props.author.avatarUrl}
+            alt={props.author.name}
+          />
+          <div className="UserInfo-name">
+            {props.author.name}
+          </div>
+        </div>
+        <div className="Comment-text">
+          {props.text}
+        </div>
+        <div className="Comment-date">
+          {formatDate(props.date)}
+        </div>
+      </div>
+    );
+  }
+*/
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h1>my-map-react</h1>
         </div>
-        <Menu entries={["Home", "Map", "About"]}/>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          A little app with React
         </p>
       </div>
     );
