6a39cdfbbf66d3e91c31ef3dc749787a9c5b8e0f
Víctor Velarde <victor.velarde@gmail.com>
2017-03-30 22:29:30 +0200

    Just reformating code

--- src/App.js
+++ src/App.js
@@ -1,71 +1,66 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
 function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
+    return <h1>Hello, {props.name}</h1>;
 }
 
-function Menu (props){
-  return(
-    <ul className="Menu">
-      { props.entries.map(e => <li><a href="#">{e}</a></li>) }
-    </ul>);
-  }
-
-  function Comment(props) {
+function Menu(props) {
     return (
-      <div className="Comment">
-        <div className="UserInfo">
-          <img className="Avatar"
-            src={props.author.avatarUrl}
-            alt={props.author.name}
-          />
-          <div className="UserInfo-name">
-            {props.author.name}
-          </div>
-        </div>
-        <div className="Comment-text">
-          {props.text}
-        </div>
-        <div className="Comment-date">
-          {props.date.toLocaleTimeString()}
-        </div>
-      </div>
+        <ul className="Menu">
+            {props.entries.map(e => <li>
+                <a href="#">{e}</a>
+            </li>)}
+        </ul>
     );
-  }
+}
+
+function Comment(props) {
+    return (
+        <div className="Comment">
+            <div className="UserInfo">
+                <img className="Avatar" src={props.author.avatarUrl} alt={props.author.name}/>
+                <div className="UserInfo-name">
+                    {props.author.name}
+                </div>
+            </div>
+            <div className="Comment-text">
+                {props.text}
+            </div>
+            <div className="Comment-date">
+                {props.date.toLocaleTimeString()}
+            </div>
+        </div>
+    );
+}
 
 const comment = {
-  date: new Date(),
-  text: "Hey ho, let's go!",
-  author: {
-    name: 'Joey Ramone',
-    avatarUrl: 'http://placekitten.com/g/64/64'
-  }
+    date: new Date(),
+    text: "Hey ho, let's go!",
+    author: {
+        name: 'Joey Ramone',
+        avatarUrl: 'http://placekitten.com/g/64/64'
+    }
 };
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>my-map-react</h1>
-        </div>
-        <p className="App-intro">
-          A little app with React
-        </p>
-        <div className="App-body">
-          <Comment
-            date={comment.date}
-            text={comment.text}
-            author={comment.author}>
-          </Comment>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1>my-map-react</h1>
+                </div>
+                <p className="App-intro">
+                    A little app with React
+                </p>
+                <div className="App-body">
+                    <Comment date={comment.date} text={comment.text} author={comment.author}></Comment>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-
 const element = <div>
-  <h1>Hey, ho! Let's go!</h1>
-  <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    <h1>Hey, ho! Let's go!</h1>
+    <h2>It is {new Date().toLocaleTimeString()}.</h2>
 </div>;
 
 ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
-);
+    <App/>, document.getElementById('root'));
 
 //setInterval(renderApp, 1000);
